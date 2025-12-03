f6bbea620f3dfd7a4758a1aeb5ff03d06ef80704
Adarrot <adam.darrot@etu.imt-nord-europe.fr>
2025-12-03 23:27:11 +0100

    Jalon4 fini, Main amélioré un peu

--- src/main/java/RayTracer/Main.java
+++ src/main/java/RayTracer/Main.java
@@ -1,27 +1,75 @@
 package RayTracer;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String GIT_ROOT_DIR = "/home/ad/Documents/Cours/POO/JavaRayTracer";
-		Scene test1 = new Scene(GIT_ROOT_DIR + "/TestScenes/jalon4/tp45.test");
-	
-		Renderer renderer = new Renderer(test1);
+		String GIT_ROOT_DIR = "/home/ad/Documents/Cours/POO/JavaRayTracer";	// répertoire du repo sur le système
 		
-		BufferedImage resultImage = renderer.render();
-	
-		saveImage(resultImage, GIT_ROOT_DIR + "/result.png");
+		String SCENES = GIT_ROOT_DIR + "/TestScenes";					// Répertoire global des scènes de test
+		String resultImagesDir = GIT_ROOT_DIR + "/ResultScenesImages";	// Répertoire où placer les nouvelles images générées
+		String[] sceneDirs = { "/jalon2", "/jalon3", "/jalon4" };	// Répertoires de scènes à traiter
 		
-		System.out.println("Image sauvegardée !");
+		
+		
+		
+		//creation du repertoire ResultScenesImages
+		try {
+			Files.createDirectories(Paths.get(resultImagesDir));
+		}catch(Exception e) {
+			System.out.println("Could not create directory " + resultImagesDir);
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		
+		Scene scene;
+		Renderer renderer;
+		BufferedImage resultImage;
+		String resultImageName;
+		
+		//generation des images de chaque jalon dans tous les sous répertoires de ResultScenesImages créés
+		for(int i=0; i<sceneDirs.length; i++) {
+			try {
+				Files.createDirectories(Paths.get(resultImagesDir + sceneDirs[i]));
+			}catch(Exception e) {
+				System.out.println("Could not create directory " + resultImagesDir + sceneDirs[i]);
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			List<String> sceneFiles = listSceneFiles(new File(SCENES+sceneDirs[i]));
+			for(int j=0; j<sceneFiles.size(); j++) {
+				System.out.println("[*] Import de " + sceneFiles.get(j));
+				
+				try {
+					scene = new Scene(SCENES + sceneDirs[i] + "/" + sceneFiles.get(j));
+					renderer = new Renderer(scene);
+					resultImage = renderer.render();
+					resultImageName = resultImagesDir + sceneDirs[i] + "/" + sceneFiles.get(j).substring(0,sceneFiles.get(j).indexOf(".")) +".png";
+					saveImage(resultImage, resultImageName);
+					System.out.println("[*]Image sauvegardée !  -> " + resultImageName);
+				}catch(Exception e) {
+					System.out.println("Erreur de génération sur la scène " + sceneFiles.get(j));
+					e.printStackTrace();
+					continue;
+				}
+				System.out.println("");
+			}
+			System.out.println("----------------------");
+		}
+		
 		
 	}
 	
@@ -38,5 +86,23 @@ public class Main {
 		
 		
 	}
+	
+	
+	
+	
+	
+	
+	private static List<String> listSceneFiles( File folder) {
+	    List<String> sceneFiles = new ArrayList();
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (!fileEntry.isDirectory()) {
+	        	if(fileEntry.getName().endsWith(".test")) {
+	        		sceneFiles.add(fileEntry.getName());
+	        	}
+	        }
+	    }
+	    return sceneFiles;
+	}
+	
 	
 }
