ab8b28450d46e386bb1100f2a954c5b7dfb67949
darrotaadam <adam.darrot@etu.imt-nord-europe.fr>
2025-12-04 10:57:31 +0100

    Main paths

--- src/main/java/RayTracer/Main.java
+++ src/main/java/RayTracer/Main.java
@@ -15,8 +15,9 @@ import javax.imageio.ImageIO;
 public class Main {
 
 	public static void main(String[] args) {
-		String GIT_ROOT_DIR = "/home/ad/Documents/Cours/POO/JavaRayTracer";	// répertoire du repo sur le système
-		
+		//String GIT_ROOT_DIR = "/home/ad/Documents/Cours/POO/JavaRayTracer";	// répertoire du repo sur le système
+		//String GIT_ROOT_DIR = "C:\\Users\\AdamDARROT\\OneDrive - ALLEO\\Bureau\\JavaRayTracer";
+		String GIT_ROOT_DIR = System.getProperty("user.dir");
 		String SCENES = GIT_ROOT_DIR + "/TestScenes";					// Répertoire global des scènes de test
 		String resultImagesDir = GIT_ROOT_DIR + "/ResultScenesImages";	// Répertoire où placer les nouvelles images générées
 		String[] sceneDirs = { "/jalon2", "/jalon3", "/jalon4" };	// Répertoires de scènes à traiter
@@ -39,6 +40,7 @@ public class Main {
 		BufferedImage resultImage;
 		String resultImageName;
 		
+		
 		//generation des images de chaque jalon dans tous les sous répertoires de ResultScenesImages créés
 		for(int i=0; i<sceneDirs.length; i++) {
 			try {
@@ -49,15 +51,15 @@ public class Main {
 				System.exit(1);
 			}
 			
-			List<String> sceneFiles = listSceneFiles(new File(SCENES+sceneDirs[i]));
+			List<String> sceneFiles = listSceneFiles(new File(Paths.get(SCENES+sceneDirs[i]).toString()) );
 			for(int j=0; j<sceneFiles.size(); j++) {
 				System.out.println("[*] Import de " + sceneFiles.get(j));
 				
 				try {
-					scene = new Scene(SCENES + sceneDirs[i] + "/" + sceneFiles.get(j));
+					scene = new Scene(Paths.get(SCENES + sceneDirs[i] + "/" + sceneFiles.get(j)).toString() );
 					renderer = new Renderer(scene);
 					resultImage = renderer.render();
-					resultImageName = resultImagesDir + sceneDirs[i] + "/" + sceneFiles.get(j).substring(0,sceneFiles.get(j).indexOf(".")) +".png";
+					resultImageName = Paths.get( resultImagesDir + sceneDirs[i] + "/" + sceneFiles.get(j).substring(0,sceneFiles.get(j).indexOf(".")) +".png").toString() ;
 					saveImage(resultImage, resultImageName);
 					System.out.println("[*]Image sauvegardée !  -> " + resultImageName);
 				}catch(Exception e) {
